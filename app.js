6813362f0247d584cd6cd61db9aa6d5cf2a311f8
Negar <negarbaharmand@outlook.com>
2025-11-03 10:39:12 +0100

    Fix port binding for Render

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import the express module to create a web server
-import express from 'express';
-import { PORT } from './config/env.js';
+import express from "express";
+import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -22,24 +22,23 @@ app.use(cookieParser());
 // Parses cookies attached to the client request and makes them available in req.cookies
 app.use(arcjetMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
-app.use('/api/v1/workflows', workflowRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 
 // Define a route for the root ("/") URL using the GET method
-app.get('/', (req, res) => {
-    // Send a simple message as a response when the root URL is accessed
-    res.send('Welcome to the Subscription Tracker API');
+app.get("/", (req, res) => {
+  // Send a simple message as a response when the root URL is accessed
+  res.send("Welcome to the Subscription Tracker API");
 });
 
 // The server doesn't start listening yet, we'll need to specify a port at the end of this file.
-app.listen(PORT, async () => { //start listening when starting the application
-    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
-    //connect to the DB
-    await connectToDatabase();
+app.listen(PORT, "0.0.0.0", async () => {
+  console.log(`Subscription Tracker API is running on port ${PORT}`);
+  await connectToDatabase();
 });
 
 export default app;
